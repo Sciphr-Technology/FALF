eaaf0064eb0d88f93bca4a63b686eb977ec3bb47
devOMAR <omar.alfarraj@yahoo.com>
2025-03-03 21:19:47 +0300

    Add Message model, migration, and factory
    Created the `messages` table migration with soft delete support. Added the `Message` model with fillable attributes and corresponding factory for generating test data. These changes enable handling and storing messages in the application database.

---
+++ database/migrations/2025_03_03_210457_create_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60)->index();
+            $table->string('email', 60)->nullable();
+            $table->string('phone', 20)->nullable();
+            $table->text('message');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
